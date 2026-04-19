a52bb6067d59f8593c3b25795eb3d22e8d2728d3
Zagyva Péter <z.peti070527@gmail.com>
2026-04-19 14:47:08 +0200

    drop test

--- drop_test/main.py
+++ drop_test/main.py
@@ -1,10 +1,25 @@
 from pathlib import Path
+import math
 
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
+    for line in data.splitlines():
+        devices, height = line.split(", ") 
+        print(min_num_of_drops(int(devices), int(height)))
 
+def min_num_of_drops(n, h):
+    if h == 1 or n == 1:
+        return h
+    
+    drops = 0
+    while True:
+        drops += 1
+        total = 0
+        for k in range(1, n + 1):
+            total += math.comb(drops, k)
+        if total >= h:
+            return drops
 
 if __name__ == "__main__":
     main()
